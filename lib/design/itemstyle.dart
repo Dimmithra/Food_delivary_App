a021208e93642f3ddc8a34e40c274ce0c5fcaacb
Lasitha Dharmaratne <kaushal@plc.lk>
2023-02-09 12:03:05 +0530

    first commits

--- lib/design/itemstyle.dart
+++ lib/design/itemstyle.dart
@@ -2,7 +2,9 @@ import 'package:burgerking_mobileapp/widget/categorywidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const boxshadow =
-    BoxShadow(color: Colors.grey, spreadRadius: 2, blurRadius: 10);
-const kratingNumber =
-    TextStyle(backgroundColor: Colors.grey, color: Colors.white, fontSize: 18);
+const boxshadow = BoxShadow(
+    color: Color.fromARGB(255, 155, 0, 0), spreadRadius: 2, blurRadius: 10);
+const kratingNumber = TextStyle(
+    backgroundColor: Color.fromARGB(255, 133, 0, 0),
+    color: Colors.white,
+    fontSize: 18);
